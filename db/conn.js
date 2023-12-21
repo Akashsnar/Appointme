df31f4a25ca704b1c54aa14d1828c748eb05a655
Akashsnar <aksn0204@gmail.com>
2023-12-21 13:08:25 +0530

    changed server

--- db/conn.js
+++ db/conn.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const client = mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
+    useNewUrlParser: true, useUnifiedTopology: false
   })
   .then(() => {
     console.log("DB connected");
